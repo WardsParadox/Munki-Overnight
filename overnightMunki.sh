e9024f7f939f7ec12ae393ef456fe4a9a27c509e
jmartinez0837 <jmartinez@riverdale.edu>
2015-03-02 11:03:22 -0500

    Update overnightMunki.sh

--- overnightMunki.sh
+++ overnightMunki.sh
@@ -3,7 +3,7 @@
 # NOTE: This Requires NoSleep https://github.com/integralpro/nosleep be installed with the "Command-Line Interface"
 # NoSleep is installed with as requires key in munki and using installer_choices_xml keys to install "Command-Line Interface"
 
-#checks for the hour of the time only; cuts to format (HH:M)
+#checks for the hour of the time only; cuts to format (HH)
 SystemHour=$(date "+TIME:: %H" | cut -c 8-9)
 
 #hour of time we have set computers to turn on (HH) done this way so it'll only run within that hour
@@ -12,26 +12,16 @@ TurnOnTime=02
 if [ "$SystemHour" = "$TurnOnTime" ] ; then
 
 		#set closing lid not to sleep laptop
-		#/usr/local/bin/NoSleepCtrl -a -b -s 1,1
+		/usr/local/bin/NoSleepCtrl -a -b -s 1,1
 	
 		#gives time for noSleep extension to load
-		/usr/bin/caffeinate sleep 5
+		sleep 5
 
 		#check system battery 
 		battPercentage=$(pmset -g batt | grep "InternalBattery-0" | cut -c 21-23 | sed s/\%//)
 		if [ "$battPercentage" -gt 50 ] ; then
-		
-			# Make sure we can talk to the server before running.
-			until /sbin/ping -c 1 h-munki.riverdale.edu; do /bin/sleep 3; done
 
 			/usr/local/munki/managedsoftwareupdate --auto
-
-			#/usr/local/munki/managedsoftwareupdate --quiet --munkistatusoutput
-			#/usr/local/munki/managedsoftwareupdate --installonly --munkistatusoutput
-			
-			#recommended by Walter
-			#/usr/local/munki/supervisor --delayrandom 90 --timeout 43200 --/usr/local/munki/managedsoftwareupdate --auto
-	
 			sleep 5
 			sudo shutdown -h now
 		else
@@ -41,7 +31,6 @@ if [ "$SystemHour" = "$TurnOnTime" ] ; then
 else
 
 	echo "Its not time to run updates."
-	
 	#set closing lid back to sleep laptop
 	/usr/local/bin/NoSleepCtrl -a -b -s 0,0
 fi

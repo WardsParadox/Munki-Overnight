bed2b486b5ade76afc1e025c5b5d2270a16ab2d2
Zack McCauley <zack.mccauley@bsd7.org>
2015-03-26 13:40:11 -0600

    Updated Logic to include the PMSET of desktops Changes: •Added a postinstall script to add a plist at /var/deployment/receipts/installs edit this •Updated plist to have installs check for this rather then a touch file method

--- overnightMunki.py
+++ overnightMunki.py
@@ -6,11 +6,11 @@ import time
 import os
 import subprocess
 import re
-import sys
+from sys import exit
 import syslog
 
 #Variables
-version = "1.0"
+version = "1.1.0"
 current_time = datetime.datetime.now().time().hour
 battery_query = re.findall(
   r'\d+%', subprocess.check_output('pmset -g batt', shell=True)
@@ -20,12 +20,12 @@ syslog.openlog("Overnight Munki Updater V %s" % version)
 
 print percentage, "% Battery"
 syslog.syslog(syslog.LOG_ALERT, "The battery is at %s %%" % percentage)
-if current_time != 01:
+if (current_time != 01 or current_time != 05):
     print "Hour of Day:", current_time
     print 'It is not time to run updates.'
     syslog.syslog(syslog.LOG_ALERT, "It is %s and is not time to run updates " % current_time)
-    sys.exit(0);
-elif percentage >= 50 and current_time == 01:
+    exit(0);
+elif percentage >= 50 and (current_time == 01 or current_time == 05) :
   # Use --auto in case laptop does go to sleep,
   # When opened there will be no visual to the user and they can still log in
   syslog.syslog(syslog.LOG_ALERT, "Running ManagedSoftwareUpdate")

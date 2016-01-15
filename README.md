a00a491a955db5eb9158855cb4be4c1c5faf4b19
Zack McCauley <zack.mccauley@bsd7.org>
2016-01-15 10:40:22 -0700

    Updated and cleaned scripts

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Munki-Overnight
-**If anyone wants to attempt to get this fully functional, feel free to fork it. Let me know and I will re-direct people there**
-
-**Please note that a majority of users are reporting that this does not fully function for them. Please test this in your environment. Also look at pmset -a lidwake 0 . Your mileage may vary.**
+Attempting to get this running again.
 
 Run Munki Overnight (so laptops can update with lid closed)
-I don't use Luggage to build my packages (removed the MakeFile and postflight, checkout a previous commit to get those back), as I run this via Munki. I have attached a munki .plist file I used to push the files out. Build a DMG with the com.example.overnightMunki.plist and the overnightMunki.py. Adjust as needed.
+Package with your package maker of choice (munki-pkg https://github.com/munki/munki-pkg)
 
 No warranties provided. Test in your environment.
 
--- overnightMunki.py
+++ overnightMunki.py
@@ -2,51 +2,52 @@
 # Libraries
 import datetime
 import time
-import os
 import subprocess
 import re
 import syslog
 import urllib2
 from sys import exit
 
-version = "1.3.0"
+version = "1.3.1"
 # Move Open Log to Front of all
 syslog.openlog("Overnight Munki Updater V %s" % version)
 #Variables
 current_time = datetime.datetime.now().time().hour
 battery_query = re.findall(
-  r'\d+%', subprocess.check_output('pmset -g batt', shell=True)
+  r'\d+%', subprocess.check_output('/usr/bin/pmset -g batt', shell=True)
 )
 percentage = ''.join(battery_query)[:-1]
+
 # Functions
-def runMunki():
-    print percentage, "%s Battery"
-    syslog.syslog(syslog.LOG_ALERT, "The battery is at %s %% " % percentage)
+def main():
+    print "{0}% Battery".format(percentage)
+    syslog.syslog(syslog.LOG_ALERT, "The battery is at %s%% " % percentage)
     if current_time != 01 and current_time != 05:
         print "Hour of Day:", current_time
         print 'It is not time to run updates.'
-        syslog.syslog(syslog.LOG_ALERT, "It is %s and is not time to run updates " % current_time)
+        syslog.syslog(syslog.LOG_ALERT,
+        "Hour is %s and is not time to run updates " % current_time)
         exit(0);
     elif percentage >= 50 :
         # Use --auto in case laptop does go to sleep,
         # When opened there will be no visual to the user and they can still log in
         syslog.syslog(syslog.LOG_ALERT, "Running ManagedSoftwareUpdate")
-        subprocess.call('/usr/local/munki/managedsoftwareupdate -v --auto',shell=True,stdout=subprocess.PIPE)
-        time.sleep(5)
-        os.system('shutdown -h now')
+        subprocess.call(['/usr/local/munki/managedsoftwareupdate','--auto'])
+        subprocess.call(['shutdown','-h','now'])
     elif percentage <= 50:
         print 'Battery too low to run updates.'
         syslog.syslog(syslog.LOG_ALERT, "The battery is too low to run updates!! Battery at %s %%. Shutting Down" % percentage)
 def internet_on():
     try:
-        response=urllib2.urlopen('http://www.google.com',timeout=1)
+        response=urllib2.urlopen('https://www.google.com/',timeout=1)
         return True
     except urllib2.URLError as err: pass
     return False
 
 # Main Run
 while True:
- if internet_on():
+    internet_on()
     break
-runMunki()
-os.system('shutdown -h now')
+if __name__ == '__main__':
+    main()
+    subprocess.call(['/sbin/shutdown','-h','now'])
